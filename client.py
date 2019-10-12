49d5f91dbd46d17eaefee794d87237f6d5f271ee
Murat Memis <h.murat.memis@gmail.com>
2019-10-12 20:07:16 +0200

    Adds Keep Alive and Raid Limiting feature

--- client.py
+++ client.py
@@ -1,8 +1,19 @@
 import socket
+import threading
+import configparser
+
+config = configparser.ConfigParser()
+config.read('conf.ini')
+
+def heartbeat(server_address):
+    connection.sendto(b'con-h 0x00', server_address)
+    heartbeat_timer = threading.Timer(3, heartbeat, [server_address])
+    heartbeat_timer.start()
 
 # Create a UDP socket
 connection = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 counter = 0
+heartbeat_timer = None
 
 server_address = ('localhost', 10000)
 
@@ -12,15 +23,23 @@ try:
     print(data)
     connection.sendto(b'com-0 accept', server_address)
 
+    if config['client']['keep_alive'] == 'yes':
+        heartbeat(server_address)
+
     while True:
-        message = input('Type your message!')
-        connection.sendto(('msg-%i=%s' % (counter, message)).encode(), server_address)
+        # message = input('Type your message!')
+        # connection.sendto(('msg-%i=%s' % (counter, message)).encode(), server_address)
 
         data, _ = connection.recvfrom(4096)
         counter += 2
 
         if data.startswith(b'res-'):
             print(data)
+        elif data.startswith(b'con-res'):
+            print('Connection was reset')
+            connection.sendto(b'con-res 0xFF', server_address)
+
+        print(data)
 
 finally:
     print('closing socket')
--- server.py
+++ server.py
@@ -1,5 +1,16 @@
 import socket
 import re
+import threading
+import time
+import configparser
+
+config = configparser.ConfigParser()
+config.read('conf.ini')
+
+def connection_reset(address):
+    print('Resetting connection')
+    print(address)
+    connection.sendto(b'con-res 0xFE', address)
 
 # Create a UDP socket
 connection = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
@@ -8,17 +19,44 @@ connection = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 server_address = ('localhost', 10000)
 print('starting up on {} port {}'.format(*server_address))
 connection.bind(server_address)
+reset_timers = {}
+packages_received = (time.time(), 0)
 
 while True:
     print('\nwaiting to receive message')
     data, address = connection.recvfrom(4096)
 
+    if time.time() == packages_received[0]:
+        if packages_received[1] < int(config['server']['pps']):
+            packages_received[1] += 1
+        else:
+            print('Received too many messages, skipping')
+            continue
+    else:
+        packages_received = (time.time(), 1)
+
     if data == b'com-0 accept':
+        reset_timers[address] = threading.Timer(4, connection_reset, [address])
+        reset_timers[address].start()
         print('received ack from {}'.format(address))
     elif data.startswith(b'com-0'):
         print('received req from {}'.format(address))
         sent = connection.sendto(('com-0 accept %s' % (socket.gethostbyname(socket.gethostname()))).encode(), address)
         print('sent ack to {}'.format(address))
     elif data.startswith(b'msg-'):
+        reset_timers[address].cancel()
         clientCounter = int(re.compile(r"msg-(\d+).*").match(data.decode()).group(1)) + 1
         sent = connection.sendto(('res-%i=I am server' % (clientCounter)).encode(), address)
+        reset_timers[address] = threading.Timer(4, connection_reset, [address])
+        reset_timers[address].start()
+    elif data.startswith(b'con-res'):
+        print('Received reset ack from Client')
+        reset_timers[address] = threading.Timer(4, connection_reset, [address])
+        reset_timers[address].start()
+    elif data.startswith(b'con-h'):
+        reset_timers[address].cancel()
+        reset_timers[address] = threading.Timer(4, connection_reset, [address])
+        reset_timers[address].start()
+
+
+
