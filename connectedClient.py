c4a187b7c42fab9e847bd6aae9bf5052fdd2b3ed
Murat Memis <h.murat.memis@gmail.com>
2019-10-24 16:27:26 +0200

    Adds class for handling connected client

---
+++ connectedClient.py
@@ -0,0 +1,22 @@
+import threading
+from enum import Enum
+
+class ConnectionState(Enum):
+    INITIAL = 1
+    HANDSHAKE_SERVER_ACK = 2
+    VERIFIED = 3
+
+class ConnectedClient:
+    address = None
+    timer = None
+    connection_state = ConnectionState.INITIAL
+
+    def __init__(self, address):
+        self.address = address
+
+    def reset_timer(self, connection_reset):
+        if self.timer is not None:
+            self.timer.cancel()
+        self.timer = threading.Timer(4, connection_reset, [self.address])
+        self.timer.start()
+
